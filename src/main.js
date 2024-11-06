e793243918d477fb2ddfc4a10e320423366fae70
fool <habitdev@163.com>
2024-11-06 11:50:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import {router} from "./router";
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 
 const app = createApp(App);
 
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 // 使用路由
 app.use(router);
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,52 @@
 import {fileURLToPath, URL} from "node:url";
 import {defineConfig, loadEnv} from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
 
-  const env = loadEnv(mode, process.cwd());
-  const { VITE_APP_ENV,VITE_APP_BASE_API } = env;
+    const env = loadEnv(mode, process.cwd());
+    const {VITE_APP_ENV, VITE_APP_BASE_API} = env;
 
-
-  return {
-    plugins: [vue()],
-
-    //配置服务器信息
-    server: {
-      cors: true, // 默认启用并允许任何源
-      open: true,
-      host: "0.0.0.0",
-      port: '8000',
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      proxy: {
-        "/api": {
-          target: VITE_APP_BASE_API,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
+    return {
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+        base: './',
+        build: {
+            outDir: `dist-${mode}`,
         },
-      },
-    },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-  }
+
+        //配置服务器信息
+        server: {
+            cors: true, // 默认启用并允许任何源
+            open: true,
+            host: "0.0.0.0",
+            port: '4000',
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            proxy: {
+                "/api": {
+                    target: VITE_APP_BASE_API,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                }
+            },
+        },
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+    }
 })
